636094ac95afc54535afb1314aaa7c8d57afd728
rajashree-guha <rajashree.625.guha@gmail.com>
2024-01-02 01:35:04 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
 import LikeImage from './components/LikeImage';
 import LikePost from './components/LikePost';
+import PropImage from './components/propImage';
+import PropPost from './components/propPost';
+
 
 function App() {
   return (
-    <div>
-      <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
-      </div>
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
+      <h1>Some Blogs using HOC</h1>
+      <LikePost/>
+      <LikeImage/>
+      <br /><br /><br />
+      <h1>Some Blogs using Render Props</h1>
+      <PropPost/>
+      <PropImage/>
     </div>
   );
 }
